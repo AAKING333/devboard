55146a5fe22fd2adcc00c8da92473e3097ee781e
Awais <zali40868@gmail.com>
2026-09-05 19:12:23 +0500

    feat: add task update and deletion

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import (Blueprint,
+                   abort,
                    render_template,
                    redirect,
                    request,
@@ -43,3 +44,84 @@ def create_task():
             return redirect(url_for('main.tasks'))
         
     return render_template('create_task.html')
+
+
+def get_task(task_id):
+    db = get_db()
+    task = db.execute(
+        "Select * from tasks Where id = ?",
+        (task_id,),
+    ).fetchone()
+    
+    if task is None:
+        abort(404)
+        
+    return task
+
+
+@main.route("/tasks/<int:task_id>/edit", methods=("GET", "POST"))
+def edit_task(task_id):
+    task = get_task(task_id)
+
+    if request.method == "POST":
+        title = request.form["title"].strip()
+        description = request.form["description"].strip()
+        priority = request.form["priority"]
+
+        if title:
+            db = get_db()
+
+            db.execute(
+                """
+                UPDATE tasks
+                SET title = ?, description = ?, priority = ?
+                WHERE id = ?
+                """,
+                (title, description, priority, task_id),
+            )
+
+            db.commit()
+
+            return redirect(url_for("main.tasks"))
+
+    return render_template(
+        "edit_task.html",
+        task=task,
+    )
+    
+    
+@main.route('/tasks/<int:task_id>/complete', methods=('POST',))
+def complete_task(task_id):
+    get_task(task_id)
+    
+    db = get_db()
+    
+    db.execute(
+        '''
+        UPDATE tasks
+        SET status == 'completed'
+        WHERE id == ?
+        ''',
+        (task_id,),
+    )
+    
+    db.commit()
+    
+    return redirect(url_for('main.tasks'))
+
+
+@main.route('/tasks/<int:task_id>/delete', methods=('POST',))
+def delete_task(task_id):
+    db = get_db()
+    
+    db.execute(
+        '''
+        Delete from tasks 
+        Where id == ?
+        ''',
+        (task_id,),
+    )
+    
+    db.commit()
+    
+    return redirect(url_for('main.tasks'))
