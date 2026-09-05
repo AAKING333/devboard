8bf1b8299ab4fcd22210aaef735aed61b8dba77e
Awais <zali40868@gmail.com>
2026-09-05 14:32:45 +0500

    feat: add task creation and listing

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,45 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint,
+                   render_template,
+                   redirect,
+                   request,
+                   url_for)
 
-main = Blueprint('main', __name__)
+from app.database import get_db
 
-@main.route('/')
+main = Blueprint("main", __name__)
+
+@main.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@main.route("/tasks")
+def tasks():
+    db = get_db()
+    tasks = db.execute("SELECT * FROM tasks ORDER BY created_at DESC").fetchall()
+    return render_template("tasks.html", tasks=tasks)
+
+@main.route("/tasks/new", methods=("GET", "POST"))
+def create_task():
+    if request.method == "POST":
+        
+        title = request.form['title'].strip()
+        description = request.form['description'].strip()
+        priority = request.form['priority']
+        
+        if title:
+            db = get_db()
+            
+            db.execute(
+                '''
+                INSERT INTO tasks (title, description, priority)
+                VALUES (?, ?, ?)
+                ''',
+                
+                (title, description, priority)
+            )
+            
+            db.commit()
+            
+            return redirect(url_for('main.tasks'))
+        
+    return render_template('create_task.html')
